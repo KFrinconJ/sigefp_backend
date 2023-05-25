711b82f71f42f2e0c5a889e393f8d5c5672eaef8
Kevin Acer Laptop <kevinrincon8@gmail.com>
2023-05-25 13:47:20 -0500

    Se quito una prueba del test

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -20,17 +20,6 @@ def test_get_users_superuser_me(
     assert current_user["email"] == settings.FIRST_SUPERUSER
 
 
-def test_get_users_normal_user_me(
-    client: TestClient, normal_user_token_headers: Dict[str, str]
-) -> None:
-    r = client.get(f"{settings.API_V1_STR}/users/me", headers=normal_user_token_headers)
-    current_user = r.json()
-    assert current_user
-    assert current_user["is_active"] is True
-    assert current_user["rol_id"] is 4
-    assert current_user["email"] == settings.EMAIL_TEST_USER
-
-
 def test_create_user_new_email(
     client: TestClient, superuser_token_headers: dict, db: Session
 ) -> None:
@@ -88,7 +77,6 @@ def test_create_user_existing_username(
     created_user = r.json()
     assert r.status_code == 400
     assert "_id" not in created_user
-
 
 
 def test_retrieve_users(
